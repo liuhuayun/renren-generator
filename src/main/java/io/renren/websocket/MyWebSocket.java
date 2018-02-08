e2cdc7ba200e2065fd9a4b9d1dee279b35373967
568656253@qq.com <568656253@qq.com>
2018-02-08 11:24:17 +0800

    websocket

--- src/main/java/io/renren/websocket/MyWebSocket.java
+++ src/main/java/io/renren/websocket/MyWebSocket.java
@@ -2,6 +2,7 @@ package io.renren.websocket;
 
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyWebSocket {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
-    private static int onlineCount = 0;
+    private static AtomicInteger onlineCount = new AtomicInteger(0);
 
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
     private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<MyWebSocket>();
@@ -30,41 +31,35 @@ public class MyWebSocket {
     public void onOpen(Session session) {
         this.session = session;
         webSocketSet.add(this);     //加入set中
-        addOnlineCount();           //在线数加1
-        System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+        System.out.println("有一连接加入！当前在线人数为" + onlineCount.incrementAndGet());
         try {
-            sendMessage(CommonConstant.CURRENT_WANGING_NUMBER.toString());
+            sendMessage("服务端消息：连接成功！");
         } catch (IOException e) {
             System.out.println("IO异常");
         }
     }
-
+    
     /**
      * 连接关闭调用的方法
+     * @throws IOException 
      */
     @OnClose
-    public void onClose() {
+    public void onClose() throws IOException {
+    	sendMessage("服务端消息：连接关闭！");
         webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
-        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+        System.out.println("有一连接关闭！当前在线人数为" + onlineCount.decrementAndGet());
     }
 
     /**
      * 收到客户端消息后调用的方法
      *
-     * @param message 客户端发送过来的消息*/
+     * @param message 客户端发送过来的消息
+     * @throws IOException */
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(String message, Session session) throws IOException {
         System.out.println("来自客户端的消息:" + message);
-
         //群发消息
-        for (MyWebSocket item : webSocketSet) {
-            try {
-                item.sendMessage(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        sendInfo("服务端群发消息："+message);
     }
 
     /**
@@ -76,35 +71,29 @@ public class MyWebSocket {
         error.printStackTrace();
     }
 
-
+    /**
+     * 给客户端发送消息
+     * @param message
+     * @throws IOException
+     */
     public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText(message);
+    	if(this.session.isOpen())
+           this.session.getBasicRemote().sendText(message);
         //this.session.getAsyncRemote().sendText(message);
     }
 
-
     /**
      * 群发自定义消息
-     * */
+     * 
+     */
     public static void sendInfo(String message) throws IOException {
         for (MyWebSocket item : webSocketSet) {
             try {
-                item.sendMessage(message);
+            	if(item.session.isOpen())
+                    item.sendMessage(message);
             } catch (IOException e) {
                 continue;
             }
         }
-    }
-
-    public static synchronized int getOnlineCount() {
-        return onlineCount;
-    }
-
-    public static synchronized void addOnlineCount() {
-        MyWebSocket.onlineCount++;
-    }
-
-    public static synchronized void subOnlineCount() {
-        MyWebSocket.onlineCount--;
     }
 }
---
+++ src/main/java/io/renren/websocket/controllet/GreetingController.java
@@ -0,0 +1,75 @@
+package io.renren.websocket.controllet;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.renren.entity.Greeting;
+
+/** 
+ * Created by haoyuyang on 2016/11/25. 
+ */  
+@RestController  
+public class GreetingController {  
+  
+    @Autowired  
+    private SimpMessageSendingOperations simpMessageSendingOperations;
+    
+    @Autowired
+	SimpMessagingTemplate template;
+    
+    
+    /** 
+     * 表示服务端可以接收客户端通过主题“/app/hello”发送过来的消息，客户端需要在主题"/topic/hello"上监听并接收服务端发回的消息 
+     * @param topic 
+     * @param headers 
+     */  
+    @MessageMapping("/hello") //"hello"为WebSocketConfig类中registerStompEndpoints()方法配置的 消息路径：接受客户端消息
+    @SendTo("/topic/greetings")  //客户端订阅消息的路径：发送给客户端消息
+    public Greeting greeting(@Header("atytopic") String topic, @Headers Map<String, Object> headers) {  
+        System.out.println(headers);
+        return new Greeting("you are connect successfully");
+    } 
+    
+    @MessageMapping("/mymsg") //"mymsg"为WebSocketConfig类中registerStompEndpoints()方法配置的消息路径  ：
+    @SendTo("/topic/feed")//客户端订阅消息的路径  ：发送给客户端消息
+    public Greeting feed(@Header("atytopic") String topic, @Headers Map<String, Object> headers) {  
+        System.out.println(headers);
+        return new Greeting("you are connect successfully2");
+    } 
+    
+    /** 
+     * 这里用的是@SendToUser，这就是发送给单一客户端的标志。本例中， 
+     * 客户端接收一对一消息的主题应该是“/user/” + 用户Id + “/message” ,这里的用户id可以是一个普通的字符串，只要每个用户端都使用自己的id并且服务端知道每个用户的id就行。 
+     * @return 
+     */  
+    @MessageMapping("/message")  
+    @SendToUser("/message")  
+    public Greeting handleSubscribe() {  
+        System.out.println("this is the @SubscribeMapping('/marco')");  
+        return new Greeting("I am a msg from SubscribeMapping('/macro').");  
+    }  
+  
+    /** 
+     * 测试对指定用户发送消息方法 
+     * @return 
+     */  
+    @RequestMapping(path = "/send", method = RequestMethod.GET)  
+    public Greeting send() {  
+//    	template.convertAndSend(arg0, arg1);
+        simpMessageSendingOperations.convertAndSendToUser("1", "/message", new Greeting("I am a msg from SubscribeMapping('/macro')."));  
+        return new Greeting("I am a msg from SubscribeMapping('/macro').");  
+    }  
+  
+  
+}  
